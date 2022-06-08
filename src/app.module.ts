d4c31d7702cc299270dfd611d3d0e5f3c7b1d68a
Lee-Sak <yo1603@naver.com>
2022-06-08 18:31:23 +0900

    add claim module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OrderController } from './order/order.controller';
 import { OrderModule } from './order/order.module';
+import { ClaimModule } from './claim/claim.module';
+import { ClaimController } from './claim/claim.controller';
 
 @Module({
-  imports: [OrderModule],
-  controllers: [AppController, OrderController],
+  imports: [OrderModule, ClaimModule],
+  controllers: [AppController, OrderController, ClaimController],
   providers: [AppService],
 })
 export class AppModule {}
