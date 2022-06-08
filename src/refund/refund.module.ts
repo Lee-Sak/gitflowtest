2e5866a6130ef823d10f8b176ac1c73127f486ea
Lee-Sak <yo1603@naver.com>
2022-06-08 18:55:20 +0900

    module test

--- src/refund/refund.module.ts
+++ src/refund/refund.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { RefundController } from './refund.controller';
 import { RefundService } from './refund.service';
-
+// 모듈테스트
 @Module({
   controllers: [RefundController],
-  providers: [RefundService]
+  providers: [RefundService],
 })
 export class RefundModule {}
