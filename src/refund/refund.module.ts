59d417d083e495517e87d706eff014f1425f5996
Lee-Sak <yo1603@naver.com>
2022-06-08 18:46:54 +0900

    add service

--- src/refund/refund.module.ts
+++ src/refund/refund.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { RefundController } from './refund.controller';
+import { RefundService } from './refund.service';
 
 @Module({
-  controllers: [RefundController]
+  controllers: [RefundController],
+  providers: [RefundService]
 })
 export class RefundModule {}
